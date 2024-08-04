65317d84e425c2d8deb8dd292b11ac8c64a4a02a
pravi <erpraveenganapathy@gmail.com>
2024-08-04 14:29:55 -0400

    update in app features

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -21,10 +21,15 @@ from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('catalog/', include('catalog.urls')),
     path('', RedirectView.as_view(url='catalog/', permanent=True)),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += [
+    path('catalog/', include('catalog.urls')),
+]
+urlpatterns += [
+    path('accounts/', include('django.contrib.auth.urls')),
+]
